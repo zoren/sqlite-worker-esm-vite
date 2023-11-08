3adfe3387d80e21413c134d9e7374573565f27e1
Søren Sjørup <soren@zoren.dk>
2023-11-08 16:46:13 +0100

    use default import

--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,4 @@
-import { default as sqlite3InitModule } from "@sqlite.org/sqlite-wasm";
+import sqlite3InitModule from "@sqlite.org/sqlite-wasm";
 
 const sqlite3 = await sqlite3InitModule();
 sqlite3.initWorker1API();
