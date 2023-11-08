a1178663f994b882c38c609eaad0cdfaa3094ea1
Søren Sjørup <soren@zoren.dk>
2023-11-08 18:08:36 +0100

    output to doc

--- src/leader.js
+++ src/leader.js
@@ -1,13 +1,35 @@
-// this attaches sqlite3Worker1Promiser to window
-import {} from '../node_modules/@sqlite.org/sqlite-wasm/sqlite-wasm/jswasm/sqlite3-worker1-promiser-bundler-friendly.js'
-import SQLiteWorker from './worker.js?worker'
+// this attaches sqlite3Worker1Promiser to window as a side effect
+import {} from "../node_modules/@sqlite.org/sqlite-wasm/sqlite-wasm/jswasm/sqlite3-worker1-promiser-bundler-friendly.js";
+import SQLiteWorker from "./worker.js?worker";
+
+const leaderOutput = document.getElementById("leaderOutput");
+
+const log = (...args) => {
+  leaderOutput.append(
+    args.map((a) => JSON.stringify(a, null, " ")).join("\n"),
+    "\n___________________________\n"
+  );
+};
+
+const demo = async () => {
+  const sql = async (type, args) => {
+    const r = await sqlite3(type, args);
+    log(type, args, r);
+    return r;
+  };
+
+  await sql("open", { filename: ":memory:" });
+  await sql("exec", `create table t1(a, b)`);
+  await sql("exec", `insert into t1 values(1, 'hello'), (2, 'world')`);
+  const r = await sql("exec", { sql: "select a, b from t1", resultRows: [] });
+  console.table(r.result.resultRows);
+};
 
 const sqlite3 = sqlite3Worker1Promiser({
   worker: () => new SQLiteWorker(),
   onerror: (...args) => console.error("worker1 promiser error", ...args),
+  onready: async () => {
+    demo();
+    window.sqlite3 = sqlite3;
+  },
 });
-// await sqlite3('open', { simulateError: true}) works!
-// await sqlite3('open', { filename: ':memory:'}) works!
-// await sqlite3('exec', 'select 5')
-window.sqlite3 = sqlite3;
-// console.log('sqlite3', sqlite3);
