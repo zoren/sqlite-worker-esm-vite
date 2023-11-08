cd857c7e7810ebab2203d111e063dae4802a6a4d
Søren Sjørup <soren@zoren.dk>
2023-11-08 13:57:49 +0100

    import worker instead

--- src/leader.js
+++ src/leader.js
@@ -1,13 +1,9 @@
 import { sqlite3Worker1Promiser } from "./esm-promiser.mjs";
+// sqlite3-worker1-bundler-friendly is not exported by @sqlite-org/sqlite-wasm so we have to import it directly
+import SQLiteWorker from './node_modules/@sqlite.org/sqlite-wasm/sqlite-wasm/jswasm/sqlite3-worker1-bundler-friendly.mjs'
 
 const sqlite3 = sqlite3Worker1Promiser({
-  worker: () =>
-    new Worker(
-      "./node_modules/@sqlite.org/sqlite-wasm/sqlite-wasm/jswasm/sqlite3-worker1-bundler-friendly.mjs",
-      {
-        type: "module",
-      }
-    ),
+  worker: () => new SQLiteWorker(),
   onerror: (...args) => console.error("worker1 promiser error", ...args),
 });
 // await sqlite3('open', { simulateError: true}) works!
